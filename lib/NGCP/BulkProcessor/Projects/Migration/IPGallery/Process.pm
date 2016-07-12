bf5639f995e913106e2ba0072124d193015b31d2
Rene Krenn <rkrenn@sipwise.com>
2016-07-12 07:44:20 +0200

    MT#18663 row bulk processing framework WIP #9
    +start implementing subscriber creation
    
    Change-Id: I8ee12037b863f878101e53d69b6ae2b44a96780b

---
+++ lib/NGCP/BulkProcessor/Projects/Migration/IPGallery/Process.pm
@@ -0,0 +1,14 @@
+package NGCP::BulkProcessor::Projects::Migration::IPGallery::Process;
+use strict;
+
+## no critic
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(
+
+);
+
+
+
+1;
