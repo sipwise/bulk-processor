c7521621aeac04f9c97a1f7657cc1ade00171739
Rene Krenn <rkrenn@sipwise.com>
2016-11-10 11:31:04 +0100

    TT#5569 contract_balance gap recovery for mr3.8
    +initial process.pl
    +contract balance gap fix thread
     +contract iteration
     +context init
     +context teardown
     +detectig gaps and overlaps
     +insert missing contract balances:
      no packages or free time considered for now
    
    +calendar implementation
    +drop URI::Encode dependency
    +for now, adopt to run with mr3.8 tables only.
    
    Change-Id: I48e0de59bb703adc0d28092c1a93a5b47432bc03

---
+++ lib/NGCP/BulkProcessor/Projects/Disaster/Balances/Check.pm
@@ -0,0 +1,110 @@
+package NGCP::BulkProcessor::Projects::Disaster::Balances::Check;
+use strict;
+
+## no critic
+
+no strict 'refs';
+
+use NGCP::BulkProcessor::Dao::mr38::billing::contracts qw();
+use NGCP::BulkProcessor::Dao::mr38::billing::contract_balances qw();
+
+#use NGCP::BulkProcessor::RestRequests::mr38::Contracts qw();
+#use NGCP::BulkProcessor::RestRequests::mr38::Customers qw();
+#use NGCP::BulkProcessor::RestRequests::mr38::BillingProfiles qw();
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(
+    check_billing_db_tables
+);
+#check_rest_get_items
+
+my $NOK = 'NOK';
+my $OK = 'ok';
+
+sub check_billing_db_tables {
+
+    my ($messages) = @_;
+
+    my $result = 1;
+    my $check_result;
+    my $message;
+
+    my $message_prefix = 'NGCP billing db tables - ';
+
+    ($check_result,$message) = _check_table($message_prefix,'NGCP::BulkProcessor::Dao::mr38::billing::contracts');
+    $result &= $check_result; push(@$messages,$message);
+
+    ($check_result,$message) = _check_table($message_prefix,'NGCP::BulkProcessor::Dao::mr38::billing::contract_balances');
+    $result &= $check_result; push(@$messages,$message);
+
+    #($check_result,$message) = _check_table($message_prefix,'NGCP::BulkProcessor::Dao::mr38::billing::lnp_providers');
+    #if (not $check_result) {
+    #    ($check_result,$message) = _check_table($message_prefix,'NGCP::BulkProcessor::Dao::mr441::billing::lnp_providers');
+    #}
+    #$result &= $check_result; push(@$messages,$message);
+
+    return $result;
+
+}
+
+
+sub _check_table {
+
+    my ($message_prefix,$module) = @_;
+    my $result = 0;
+    my $message = ($message_prefix // '') . &{$module . '::gettablename'}() . ': ';
+    eval {
+        $result = &{$module . '::check_table'}();
+    };
+    if (@$ or not $result) {
+        return (0,$message . $NOK);
+    } else {
+        return (1,$message . $OK);
+    }
+
+}
+
+#sub check_rest_get_items {
+#
+#    my ($messages) = @_;
+#
+#    my $result = 1;
+#    my $check_result;
+#    my $message;
+#
+#    my $message_prefix = 'NGCP id\'s/constants - ';
+#
+#    ($check_result,$message, my $reseller) = _check_rest_get_item($message_prefix,
+#        'NGCP::BulkProcessor::RestRequests::mr38::Resellers',
+#        $reseller_id,
+#        'name');
+#    $result &= $check_result; push(@$messages,$message);
+#
+#
+#    return $result;
+#
+#}
+
+sub _check_rest_get_item {
+
+    my ($message_prefix,$module,$id,$item_name_field,$get_method,$item_path_method) = @_;
+    my $item = undef;
+    $get_method //= 'get_item';
+    $item_path_method //= 'get_item_path';
+    my $message = ($message_prefix // '') . &{$module . '::' . $item_path_method}($id) . ': ';
+    return (0,$message . $NOK,$item) unless $id;
+    eval {
+        $item = &{$module . '::' . $get_method}($id);
+    };
+
+    if (@$ or not defined $item or ('ARRAY' eq ref $item and (scalar @$item) != 1)) {
+        return (0,$message . $NOK,$item);
+    } else {
+        $item = $item->[0] if ('ARRAY' eq ref $item and (scalar @$item) == 1);
+        return (1,$message . "'" . $item->{$item_name_field} . "' " . $OK,$item);
+    }
+
+}
+
+1;
