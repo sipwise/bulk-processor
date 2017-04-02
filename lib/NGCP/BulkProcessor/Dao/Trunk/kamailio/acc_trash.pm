1dd384ede5dc2bbad8affb1268faf6572cd005e9
Rene Krenn <rkrenn@sipwise.com>
2017-04-02 15:28:59 +0200

    TT#13821 process acc trash records
    fix tool to update acc_trash and re-enqueue records accumulated
    during 3.x->4.x updates due to introduction of cdr lnp prefix
    fields.
    
    Change-Id: Iea6140edccc1aeb456f6584b7d8335e18ba1b69d

---
+++ lib/NGCP/BulkProcessor/Dao/Trunk/kamailio/acc_trash.pm
@@ -0,0 +1,169 @@
+package NGCP::BulkProcessor::Dao::Trunk::kamailio::acc_trash;
+use strict;
+
+## no critic
+
+use NGCP::BulkProcessor::Logging qw(
+    getlogger
+    rowsdeleted
+);
+
+use NGCP::BulkProcessor::ConnectorPool qw(
+    get_kamailio_db
+);
+
+use NGCP::BulkProcessor::SqlProcessor qw(
+    checktableinfo
+    process_table
+    copy_row
+);
+use NGCP::BulkProcessor::SqlRecord qw();
+
+require Exporter;
+our @ISA = qw(Exporter NGCP::BulkProcessor::SqlRecord);
+our @EXPORT_OK = qw(
+    gettablename
+    check_table
+
+    process_records
+    delete_ids
+);
+
+my $tablename = 'acc_trash';
+my $get_db = \&get_kamailio_db;
+
+my $expected_fieldnames = [
+"id",
+"method",
+"from_tag",
+"to_tag",
+"callid",
+"sip_code",
+"sip_reason",
+"time",
+"time_hires",
+"src_leg",
+"dst_leg",
+"dst_user",
+"dst_ouser",
+"dst_domain",
+"src_user",
+"src_domain",
+];
+
+my $indexes = {};
+
+my $insert_unique_fields = [];
+
+sub new {
+
+    my $class = shift;
+    my $self = NGCP::BulkProcessor::SqlRecord->new($class,$get_db,
+                           $tablename,$expected_fieldnames,$indexes);
+
+    copy_row($self,shift,$expected_fieldnames);
+
+    return $self;
+
+}
+
+
+sub delete_ids {
+
+    my ($xa_db,$ids) = @_;
+
+    check_table();
+    my $db = &$get_db();
+    $xa_db //= $db;
+    my $table = $db->tableidentifier($tablename);
+
+    my $stmt = 'DELETE FROM ' . $table . ' WHERE ' .
+            $db->columnidentifier('id') . ' IN (' . substr(',?' x scalar @$ids,1) . ')';
+    my @params = @$ids;
+
+    my $count;
+    if ($count = $xa_db->db_do($stmt,@params)) {
+        rowsdeleted($db,$tablename,$count,$count,getlogger(__PACKAGE__));
+        return 1;
+    } else {
+        rowsdeleted($db,$tablename,0,0,getlogger(__PACKAGE__));
+        return 0;
+    }
+
+}
+
+
+sub process_records {
+
+    my %params = @_;
+    my ($process_code,
+        $static_context,
+        $init_process_context_code,
+        $uninit_process_context_code,
+        $multithreading,
+        $numofthreads,
+        $load_recursive) = @params{qw/
+            process_code
+            static_context
+            init_process_context_code
+            uninit_process_context_code
+            multithreading
+            numofthreads
+            load_recursive
+        /};
+
+    check_table();
+
+    return process_table(
+        get_db                      => $get_db,
+        class                       => __PACKAGE__,
+        process_code                => sub {
+                my ($context,$rowblock,$row_offset) = @_;
+                return &$process_code($context,buildrecords_fromrows($rowblock,$load_recursive),$row_offset);
+            },
+        static_context              => $static_context,
+        init_process_context_code   => $init_process_context_code,
+        uninit_process_context_code => $uninit_process_context_code,
+        destroy_reader_dbs_code     => \&destroy_dbs,
+        multithreading              => $multithreading,
+        tableprocessing_threads     => $numofthreads,
+    );
+}
+
+sub buildrecords_fromrows {
+
+    my ($rows,$load_recursive) = @_;
+
+    my @records = ();
+    my $record;
+
+    if (defined $rows and ref $rows eq 'ARRAY') {
+        foreach my $row (@$rows) {
+            $record = __PACKAGE__->new($row);
+
+            # transformations go here ...
+
+            push @records,$record;
+        }
+    }
+
+    return \@records;
+
+}
+
+sub gettablename {
+
+    return $tablename;
+
+}
+
+sub check_table {
+
+    return checktableinfo($get_db,
+                   __PACKAGE__,$tablename,
+                   $expected_fieldnames,
+                   $indexes);
+
+}
+
+1;
---
+++ lib/NGCP/BulkProcessor/Projects/Disaster/Acc/AccTrash.pm
@@ -0,0 +1,340 @@
+package NGCP::BulkProcessor::Projects::Disaster::Acc::AccTrash;
+use strict;
+
+## no critic
+
+use threads::shared qw();
+#use List::Util qw();
+use DateTime qw();
+
+use NGCP::BulkProcessor::Projects::Disaster::Acc::Settings qw(
+    $dry
+    $skip_errors
+
+    $process_acc_trash_multithreading
+    $process_acc_trash_numofthreads
+);
+#$set_preference_bulk_numofthreads
+
+use NGCP::BulkProcessor::Logging qw (
+    getlogger
+    processing_info
+    processing_debug
+);
+use NGCP::BulkProcessor::LogError qw(
+    rowprocessingerror
+    rowprocessingwarn
+);
+
+use NGCP::BulkProcessor::Dao::trunk::accounting::cdr qw();
+use NGCP::BulkProcessor::Dao::trunk::kamailio::acc qw();
+use NGCP::BulkProcessor::Dao::trunk::kamailio::acc_trash qw();
+
+
+
+use NGCP::BulkProcessor::ConnectorPool qw(
+    get_xa_db
+);
+
+use NGCP::BulkProcessor::ConnectorPool qw(
+    destroy_dbs
+);
+
+use NGCP::BulkProcessor::Utils qw(threadid);
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(
+    process_acc_trash
+);
+
+sub process_acc_trash {
+
+    my $static_context = {};
+    my $result = _process_acc_trash_checks($static_context);
+
+    destroy_dbs();
+    my $warning_count :shared = 0;
+    return ($result && NGCP::BulkProcessor::Dao::trunk::kamailio::acc_trash::process_records(
+        static_context => $static_context,
+        process_code => sub {
+            my ($context,$records,$row_offset) = @_;
+            my $rownum = $row_offset;
+            foreach my $acc_trash (@$records) {
+                $rownum++;
+                next unless _reset_process_acc_trash_context($context,$acc_trash,$rownum);
+                _process_acc_trash($context);
+            }
+
+            #return 0;
+            return 1;
+        },
+        init_process_context_code => sub {
+            my ($context)= @_;
+            $context->{db} = &get_xa_db();
+            $context->{error_count} = 0;
+            $context->{warning_count} = 0;
+            # below is not mandatory..
+            _check_insert_tables();
+        },
+        uninit_process_context_code => sub {
+            my ($context)= @_;
+            undef $context->{db};
+            destroy_dbs();
+            {
+                lock $warning_count;
+                $warning_count += $context->{warning_count};
+            }
+        },
+        load_recursive => 0,
+        multithreading => $process_acc_trash_multithreading,
+        numofthreads => $process_acc_trash_numofthreads,
+    ),$warning_count);
+}
+
+
+sub _check_insert_tables {
+
+    #NGCP::BulkProcessor::Dao::mr38::provisioning::voip_usr_preferences::check_table();
+
+}
+
+sub _process_acc_trash {
+    my ($context) = @_;
+
+    eval {
+        $context->{db}->db_begin();
+        #my $last_balance = undef;
+        #foreach my $contract_balance (sort NGCP::BulkProcessor::Dao::mr38::billing::contract_balances::sort_by_end @{$context->{contract_balances}}) {
+        #    #print "  " . $contract_balance->{id} . " " . $contract_balance->{_start} . ' ' . $contract_balance->{_end} . "\n";
+        #    if (defined $last_balance) {
+        #        my $gap_start = $last_balance->{_end}->clone->add(seconds => 1);
+        #        my $gap_end = $contract_balance->{_start};
+        #        my $date_comparison = DateTime->compare($gap_start, $gap_end);
+        #        if ($date_comparison > 0) {
+        #            if ($skip_errors) {
+        #                _warn($context,"($context->{rownum}) " . 'contract balances overlap for contract id ' . $context->{contract}->{id} . ' detected: '.
+        #                $gap_start . ' - ' . $gap_end);
+        #            } else {
+        #                _error($context,"($context->{rownum}) " . 'contract balances overlap for contract id ' . $context->{contract}->{id} . ' detected: '.
+        #                $gap_start . ' - ' . $gap_end);
+        #            }
+        #        } elsif ($date_comparison < 0) {
+        #            _info($context,"($context->{rownum}) " . 'contract balances gap for contract id ' . $context->{contract}->{id} . ' detected: '.
+        #                $gap_start . ' - ' . $gap_end);
+        #            _insert_contract_balances($context,$gap_start,$gap_end->clone->subtract(seconds => 1),$contract_balance);
+        #        }
+        #    }
+        #    $last_balance = $contract_balance;
+        #}
+
+        #if ($dry) {
+        #    $context->{db}->db_rollback(0);
+        #} else {
+        #    $context->{db}->db_commit();
+        #}
+
+    };
+    my $err = $@;
+    if ($err) {
+        eval {
+            $context->{db}->db_rollback(1);
+        };
+        if ($skip_errors) {
+            _warn($context,"($context->{rownum}) " . 'database error with acc trash id ' . $context->{acc_trash}->{id} . ': ' . $err);
+        } else {
+            _error($context,"($context->{rownum}) " . 'database error with acc trash id ' . $context->{acc_trash}->{id} . ': ' . $err);
+        }
+    }
+}
+
+#sub _insert_contract_balances {
+#    my ($context,$gap_start,$gap_end,$contract_balance) = @_;
+#
+#    my $start = $gap_start;
+#    my $last_end;
+#    my $end;
+#    while (($end = $start->clone->add(months => 1)->subtract(seconds => 1)) <= $gap_end) {
+#        my $billing_mapping = NGCP::BulkProcessor::Dao::mr38::billing::billing_mappings::findby_contractid_ts($context->{db},$context->{contract}->{id},$start)->[0];
+#        if (defined $billing_mapping) {
+#            #todo: check if billing profile is postpaid, has zero free_time and free_cash.
+#            #todo: contracts with profile packages defining intervals other than 1 month are not supported atm.
+#            #todo: dynamically choose mr38/4x contract_balance table dao.
+#            $last_end = $end;
+#            #_insert_contract_balances($context,$gap_start,$gap_end,$contract_balance,$billing_mapping);
+#            my $id = NGCP::BulkProcessor::Dao::mr38::billing::contract_balances::insert_row($context->{db},{
+#                contract_id => $context->{contract}->{id},
+#                start => $context->{db}->datetime_to_string($start),
+#                end => $context->{db}->datetime_to_string($end),
+#                cash_balance => 0,
+#                free_time_balance => 0,
+#            });
+#            _info($context,"($context->{rownum}) " . 'contract balance id ' . $id . ' for contract id ' . $context->{contract}->{id} . ' inserted: '.
+#                $start . ' - ' . $end);
+#        } else {
+#            if ($skip_errors) {
+#                _warn($context,"($context->{rownum}) " . 'no billing mapping for contract id ' . $context->{contract}->{id} . ', t = ' . $start . ' found ');
+#            } else {
+#                _error($context,"($context->{rownum}) " . 'no billing mapping for contract id ' . $context->{contract}->{id} . ', t = ' . $start . ' found ');
+#            }
+#        }
+#        $start = $end->clone->add(seconds => 1);
+#    }
+#    if (not defined $last_end or DateTime->compare($last_end, $gap_end) != 0) {
+#        if ($skip_errors) {
+#            _warn($context,"($context->{rownum}) " . 'contract balances gap for contract id ' . $context->{contract}->{id} . ' cannot be filled with monthly intervals');
+#        } else {
+#            _error($context,"($context->{rownum}) " . 'contract balances gap for contract id ' . $context->{contract}->{id} . ' cannot be filled with monthly intervals');
+#        }
+#    }
+#
+#}
+
+sub _process_acc_trash_checks {
+    my ($context) = @_;
+
+    my $result = _checks($context);
+
+    return $result;
+}
+
+sub _reset_process_acc_trash_context {
+
+    my ($context,$acc_trash,$rownum) = @_;
+
+    my $result = _reset_context($context,$acc_trash,$rownum);
+
+    #$context->{contract_balances} = NGCP::BulkProcessor::Dao::mr38::billing::contract_balances::findby_contractid($context->{db},$context->{contract}->{id});
+
+    ##$context->{barring_profile} = $imported_subscriber->{barring_profile};
+    ##$context->{ncos_level} = $context->{ncos_level_map}->{$context->{barring_profile}};
+
+    ##delete $context->{adm_ncos_id_preference_id};
+
+    return $result;
+
+}
+
+
+sub _checks  {
+
+    my ($context) = @_;
+
+    my $result = 1;
+    #my $optioncount = 0;
+    #eval {
+    #    $optioncount = NGCP::BulkProcessor::Projects::Migration::IPGallery::Dao::import::FeatureOption::countby_subscribernumber_option();
+    #};
+    #if ($@ or $optioncount == 0) {
+    #    rowprocessingerror(threadid(),'please import subscriber features first',getlogger(__PACKAGE__));
+    #    $result = 0; #even in skip-error mode..
+    #}
+    #my $userpasswordcount = 0;
+    #eval {
+    #    $userpasswordcount = NGCP::BulkProcessor::Projects::Migration::IPGallery::Dao::import::UsernamePassword::countby_fqdn();
+    #};
+    #if ($@ or $userpasswordcount == 0) {
+    #    rowprocessingerror(threadid(),'please import user passwords first',getlogger(__PACKAGE__));
+    #    $result = 0; #even in skip-error mode..
+    #}
+    #my $subscribercount = 0;
+    #my $subscriber_barring_profiles = [];
+    #eval {
+    #    $subscribercount = NGCP::BulkProcessor::Projects::Migration::IPGallery::Dao::import::Subscriber::countby_subscribernumber();
+    #    $subscriber_barring_profiles = NGCP::BulkProcessor::Projects::Migration::IPGallery::Dao::import::Subscriber::list_barringprofiles();
+    #};
+    #if ($@ or $subscribercount == 0) {
+    #    rowprocessingerror(threadid(),'please import subscribers first',getlogger(__PACKAGE__));
+    #    $result = 0; #even in skip-error mode..
+    #}
+
+    return $result;
+
+}
+
+sub _reset_context {
+
+    my ($context,$acc_trash,$rownum) = @_;
+
+    my $result = 1;
+
+    $context->{rownum} = $rownum;
+
+    $context->{acc_trash} = $acc_trash;
+
+    #$context->{cli} = $imported_subscriber->subscribernumber();
+    #$context->{e164} = {};
+    #$context->{e164}->{cc} = substr($context->{cli},0,3);
+    #$context->{e164}->{ac} = '';
+    #$context->{e164}->{sn} = substr($context->{cli},3);
+
+    #$context->{subscriberdelta} = $imported_subscriber->{delta};
+
+    #my $userpassword = NGCP::BulkProcessor::Projects::Migration::IPGallery::Dao::import::UsernamePassword::findby_fqdn($context->{cli});
+    #if (defined $userpassword) {
+    #    $context->{username} = (defined $subsciber_username_prefix ? $subsciber_username_prefix : '') . $userpassword->{username};
+    #    $context->{password} = $userpassword->{password};
+    #    $context->{userpassworddelta} = $userpassword->{delta};
+    #} else {
+    #    # once full username+passwords is available:
+    #    delete $context->{username};
+    #    delete $context->{password};
+    #    delete $context->{userpassworddelta};
+    #    if ($context->{subscriberdelta} eq
+    #        $NGCP::BulkProcessor::Projects::Migration::IPGallery::Dao::import::Subscriber::deleted_delta) {
+    #
+    #    } else {
+    #        $result &= 0;
+    #
+    #        # for now, as username+passwords are incomplete:
+    #        #$context->{username} = $context->{e164}->{sn};
+    #        #$context->{password} = $context->{username};
+    #        #$context->{userpassworddelta} = $NGCP::BulkProcessor::Projects::Migration::IPGallery::Dao::import::UsernamePassword::updated_delta;
+    #
+    #        if ($skip_errors) {
+    #            # for now, as username+passwords are incomplete:
+    #            _warn($context,"($context->{rownum}) " . 'no username/password for subscriber found: ' . $context->{cli});
+    #        } else {
+    #            _error($context,"($context->{rownum}) " . 'no username/password for subscriber found: ' . $context->{cli});
+    #        }
+    #    }
+    #}
+    #
+    #delete $context->{billing_voip_subscriber};
+    #delete $context->{provisioning_voip_subscriber};
+
+    return $result;
+
+}
+
+
+
+
+
+sub _error {
+
+    my ($context,$message) = @_;
+    $context->{error_count} = $context->{error_count} + 1;
+    rowprocessingerror($context->{tid},$message,getlogger(__PACKAGE__));
+
+}
+
+sub _warn {
+
+    my ($context,$message) = @_;
+    $context->{warning_count} = $context->{warning_count} + 1;
+    rowprocessingwarn($context->{tid},$message,getlogger(__PACKAGE__));
+
+}
+
+sub _info {
+
+    my ($context,$message,$debug) = @_;
+    if ($debug) {
+        processing_debug($context->{tid},$message,getlogger(__PACKAGE__));
+    } else {
+        processing_info($context->{tid},$message,getlogger(__PACKAGE__));
+    }
+}
+
+1;
