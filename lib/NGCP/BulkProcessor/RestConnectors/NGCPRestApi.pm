d97c461e394e85a75cb8ba6f0ec0cd9207f96820
Rene Krenn <rkrenn@sipwise.com>
2017-05-10 02:51:42 +0200

    TT#18878 TT#11802 implement Teletek/generic importer WIP
    - CSV importer: escaped field enclosing, mutliline values,
      comment char, constant memory
    - SQLite table to hold flat file content
    - multithread import for large files
    - concept of single flatfile abandonend as teletek also wants
      to import AllowedIPs, MaxConcurrentCalls, NCOS levels,
      CallForwards
    - load teletek's main subscriber flatfile
    - single insert/upsert txn import to identify duplicate
      cc+ac+sn
    - alias range unfolding
    - alias range unfolding fix, analysis of duplicate cc+ac+sn
      in export_kundinfo_leica2.csv
    - fixed one-off bug in parser
    - aliases sorting
    - aliases dupe check
    - check for sip account used by different contacts
    - reseller cache/check for missing reseller
    - domain cache/check for missing domain
    - billing profile cache/check for missing billing profile
    - rest api cert-authentification
    
    Change-Id: I4f5ae61610bab744e3d0644f7a56afa8874a2c3c

--- lib/NGCP/BulkProcessor/RestConnectors/NGCPRestApi.pm
+++ lib/NGCP/BulkProcessor/RestConnectors/NGCPRestApi.pm
@@ -9,8 +9,12 @@ use threads::shared qw(shared_clone);
 use HTTP::Status qw(:constants :is status_message);
 
 use JSON qw();
+use IO::Uncompress::Unzip qw();
 
-use NGCP::BulkProcessor::Globals qw($LongReadLen_limit);
+use NGCP::BulkProcessor::Globals qw(
+    $LongReadLen_limit
+    $working_path
+);
 use NGCP::BulkProcessor::Logging qw(
     getlogger
     restdebug
@@ -20,22 +24,30 @@ use NGCP::BulkProcessor::LogError qw(
     resterror
     restwarn
     restrequesterror
-    restresponseerror);
+    restresponseerror
+    fileerror
+);
 
 use NGCP::BulkProcessor::RestConnector qw(_add_headers);
 
 use NGCP::BulkProcessor::Calendar qw(get_fake_now_string);
 
+use NGCP::BulkProcessor::Utils qw(makedir cleanupdir);
+
 require Exporter;
 our @ISA = qw(Exporter NGCP::BulkProcessor::RestConnector);
 our @EXPORT_OK = qw(
     $ITEM_REL_PARAM
+    cleanupcertfiles
 );
 
 my $defaulturi = 'https://127.0.0.1:443';
 my $defaultusername = 'administrator';
 my $defaultpassword = 'administrator';
 my $defaultrealm = 'api_admin_http';
+my $defaulcrtauth = 1;
+my $API_CERT_DIR = 'apicerts/';
+my $API_CERT_FILENAME_FORMAT = '%s%s.pem';
 
 my $default_collection_page_size = 10;
 my $first_page_num = 1;
@@ -48,6 +60,16 @@ my $faketime_header = 'X-Fake-Clienttime';
 
 our $ITEM_REL_PARAM = 'item_rel';
 #my $logger = getlogger(__PACKAGE__);
+
+sub _get_api_cert_dir {
+    return $working_path . $API_CERT_DIR;
+}
+
+sub cleanupcertfiles {
+
+    return cleanupdir(_get_api_cert_dir(),1,\&filewarn,getlogger(__PACKAGE__));
+
+}
 
 sub new {
 
@@ -68,12 +90,20 @@ sub new {
 sub setup {
 
     my $self = shift;
-    my ($baseuri,$username,$password,$realm,$faketime) = @_;
+    my ($baseuri,$username,$password,$realm,$crtauth,$faketime) = @_;
     $self->baseuri($baseuri // $defaulturi);
     $self->{username} = $username // $defaultusername;
     $self->{password} = $password // $defaultpassword;
     $self->{realm} = $realm // $defaultrealm;
     $self->{faketime} = $faketime // $defaultfaketime;
+    $self->{crtauth} = $crtauth // $defaultcrtauth;
+
+    if ($self->{crtauth}) {
+        makedir(_get_api_cert_dir(),\&fileerror,getlogger(__PACKAGE__));
+        $self->{crt_path} = sprintf($API_CERT_FILENAME_FORMAT,_get_api_cert_dir(),$self->{username}=~s/[^a-z0-9 _]/replacement/gir);
+    } else {
+        $self->{crt_path} = undef;
+    }
 
 }
 
@@ -99,8 +129,53 @@ sub _setup_ua {
     if ($self->{username}) {
         $ua->credentials($netloc, $self->{realm}, $self->{username}, $self->{password});
     }
+    $self->_init_ssl_cert($ua) if $self->{crtauth};
     restdebug($self,"ua configured",getlogger(__PACKAGE__));
 
+}
+
+sub _init_ssl_cert {
+    my ($self, $ua) = @_;
+    unless(-f $self->{crt_path}) {
+        my $res = $ua->post(
+            $self->_get_request_uri('/api/admincerts/')
+            Content_Type => $contenttype,
+            Content => '{}'
+        );
+        if ($res->is_success) {
+            my $zip = $res->decoded_content;
+            my $z = IO::Uncompress::Unzip->new(\$zip, MultiStream => 0, Append => 1);
+            my $data;
+            while(!$z->eof() && (my $hdr = $z->getHeaderInfo())) {
+                unless($hdr->{Name} =~ /\.pem$/) {
+                    # wrong file, just read stream, clear buffer and try next
+                    while($z->read($data) > 0) {}
+                    $data = undef;
+                    $z->nextStream();
+                    next;
+                }
+                while($z->read($data) > 0) {}
+                last;
+            }
+            $z->close();
+            if ($data) {
+                if (open(my $fh,">:raw",$self->{crt_path})) {
+                    print $fh $data;
+                    close $fh;
+                } else {
+                    fileerror("failed to open $self->{crt_path}: $!",getlogger(__PACKAGE__));
+                }
+            } else {
+                resterror($self,'failed to find PEM file in client certificate zip file',getlogger(__PACKAGE__));
+            }
+        } else {
+            resterror($self,'failed to fetch client certificate: '. $res->code . ' ' . $res->message,getlogger(__PACKAGE__));
+        }
+    }
+    $ua->ssl_opts(
+        SSL_cert_file => $self->{crt_path},
+        SSL_key_file => $self->{crt_path},
+    );
 }
 
 sub _encode_request_content {
