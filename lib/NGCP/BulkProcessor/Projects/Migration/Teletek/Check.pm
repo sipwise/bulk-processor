62d38ce489d6505a5a1b6bf68599f2a7e0724dc2
Rene Krenn <rkrenn@sipwise.com>
2017-09-13 12:51:20 +0200

    TT#18879 implement teletek importer #5
    + introduce support to manipulate contract, domain, etc.
      voip_preferences
    + write concurrent_max_total preference
    + error in invalid "channels = 0"
    
    + set emtpy web_password, if web_username is empty
    + auto-generate web_username, if duplicate
    + auto-generate web_password, if web_username is not emtpy but
      web_password is empty
    
    + parse export_CLIR.csv
    + write clir preference
    
    + write allowed_ips_grp preference
    
    + use maximum of concurrent_max_total for a subscriber,
      when inconsisten
    + use combined list of allowed ips, when inconsistent
    
    Change-Id: I26bab2cb9b4ef349777ebef2e883c9e41af3eeba

--- lib/NGCP/BulkProcessor/Projects/Migration/Teletek/Check.pm
+++ lib/NGCP/BulkProcessor/Projects/Migration/Teletek/Check.pm
@@ -40,6 +40,8 @@ use NGCP::BulkProcessor::Dao::Trunk::provisioning::voip_cf_mappings qw();
 use NGCP::BulkProcessor::Dao::Trunk::kamailio::voicemail_users qw();
 
 use NGCP::BulkProcessor::Projects::Migration::Teletek::Dao::import::Subscriber qw();
+use NGCP::BulkProcessor::Projects::Migration::Teletek::Dao::import::AllowedCli qw();
+use NGCP::BulkProcessor::Projects::Migration::Teletek::Dao::import::Clir qw();
 
 use NGCP::BulkProcessor::RestRequests::Trunk::Resellers qw();
 use NGCP::BulkProcessor::RestRequests::Trunk::Domains qw();
@@ -127,6 +129,12 @@ sub check_import_db_tables {
     my $message_prefix = 'import db tables - ';
 
     ($check_result,$message) = _check_table($message_prefix,'NGCP::BulkProcessor::Projects::Migration::Teletek::Dao::import::Subscriber');
+    $result &= $check_result; push(@$messages,$message);
+
+    ($check_result,$message) = _check_table($message_prefix,'NGCP::BulkProcessor::Projects::Migration::Teletek::Dao::import::AllowedCli');
+    $result &= $check_result; push(@$messages,$message);
+
+    ($check_result,$message) = _check_table($message_prefix,'NGCP::BulkProcessor::Projects::Migration::Teletek::Dao::import::Clir');
     $result &= $check_result; push(@$messages,$message);
 
     return $result;
