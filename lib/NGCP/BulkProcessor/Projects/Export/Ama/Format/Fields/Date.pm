dcd211b9b8e839e92e8dcbfe3a0cd428cc17a46f
Rene Krenn <rkrenn@sipwise.com>
2018-10-25 01:41:26 +0200

    TT#46262 AMA format writer objects
    Change-Id: Ifa577ba48531e4410e62b3b6ed70edb2be10dd17

---
+++ lib/NGCP/BulkProcessor/Projects/Export/Ama/Format/Fields/Date.pm
@@ -0,0 +1,59 @@
+package NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::Date;
+use strict;
+
+## no critic
+
+use NGCP::BulkProcessor::Projects::Export::Ama::Format::Field qw($TERMINATOR);
+
+#use NGCP::BulkProcessor::Utils qw(zerofill);
+use NGCP::BulkProcessor::Calendar qw(datetime_from_string);
+
+require Exporter;
+our @ISA = qw(Exporter NGCP::BulkProcessor::Projects::Export::Ama::Format::Field);
+our @EXPORT_OK = qw(
+    get_ama_date
+);
+
+my $field_name = "date";
+my $length = 6;
+my @param_names = qw/date/;
+
+sub new {
+
+    my $class = shift;
+    my $self = NGCP::BulkProcessor::Projects::Export::Ama::Format::Field->new(
+        $class,
+        name => $field_name,
+        length => $length,
+        @_);
+    #$self->{padding} //= 0;
+    #$self->{sensor_id} = substr($self->{sensor_id},1( if defined $self->{sensor_id};
+
+    return $self;
+
+}
+
+sub _get_param_names {
+
+    my $self = shift;
+    return @param_names;
+
+}
+
+sub get_hex {
+
+    my $self = shift;
+    my ($date) = $self->_get_params(@_);
+    die("invalid date '$date'") unless length($date) == 5;
+    return $date . $TERMINATOR;
+
+}
+
+sub get_ama_date {
+
+    my $dt = datetime_from_string(shift);
+    return substr($dt->year(),-1) . sprintf('%.2d',$dt->month()) . sprintf('%.2d',$dt->day());
+
+}
+
+1;
