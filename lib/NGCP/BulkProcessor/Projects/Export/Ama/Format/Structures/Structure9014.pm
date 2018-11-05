f0b24a835b7b093ef8bdb3b57c74032ac82db05d
Rene Krenn <rkrenn@sipwise.com>
2018-11-05 01:01:02 +0100

    TT#46752 AMA format writer objects: structure 9013, 9014 WIP
    Change-Id: I93498b1bcdb6bd7923b1f1c1d49a76ae0c4b8b0e

---
+++ lib/NGCP/BulkProcessor/Projects/Export/Ama/Format/Structures/Structure9014.pm
@@ -0,0 +1,89 @@
+package NGCP::BulkProcessor::Projects::Export::Ama::Format::Structures::Structure9014;
+use strict;
+
+## no critic
+
+use NGCP::BulkProcessor::Projects::Export::Ama::Format::Structure qw();
+
+use NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::HexId qw();
+use NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::StructureCode qw($STRUCTURE_CODE_9014);
+use NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::CallTypeCode qw();
+use NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::SensorType qw();
+use NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::SensorId qw();
+use NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::RecordingOfficeType qw();
+use NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::RecordingOfficeId qw();
+use NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::Date qw();
+use NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::Time qw();
+use NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::RecorderGenericIssue qw();
+use NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::TracerType qw();
+use NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::SequenceNumber qw();
+
+require Exporter;
+our @ISA = qw(Exporter NGCP::BulkProcessor::Projects::Export::Ama::Format::FieldSet NGCP::BulkProcessor::Projects::Export::Ama::Format::Structure);
+our @EXPORT_OK = qw(
+
+);
+
+my $length = 78;
+
+sub new {
+
+    my $class = shift;
+    my $self = NGCP::BulkProcessor::Projects::Export::Ama::Format::Structure->new(
+        $class,
+        length => $length,
+        #structure_name => $structure_name,
+        @_);
+    $self->_add_field(NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::HexId->new(
+        @_,
+    ));
+    $self->{structure_code} = NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::StructureCode->new(
+        structure_code => $STRUCTURE_CODE_9014,
+        @_,
+    );
+    $self->_add_field($self->{structure_code});
+    $self->_add_field(NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::CallTypeCode->new(
+        @_,
+    ));
+    $self->_add_field(NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::SensorType->new(
+        @_,
+    ));
+    $self->_add_field(NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::SensorId->new(
+        @_,
+    ));
+    $self->_add_field(NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::RecordingOfficeType->new(
+        @_,
+    ));
+    $self->_add_field(NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::RecordingOfficeId->new(
+        @_,
+    ));
+    $self->_add_field(NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::Date->new(
+        @_,
+    ));
+    $self->_add_field(NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::Time->new(
+        @_,
+    ));
+    $self->_add_field(NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::RecorderGenericIssue->new(
+        @_,
+    ));
+    $self->_add_field(NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::TracerType->new(
+        @_,
+    ));
+    $self->_add_field(NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::SequenceNumber->new(
+        @_,
+    ));
+
+    return $self;
+
+}
+
+sub get_structure_code_field {
+    my $self = shift;
+    return $self->{structure_code};
+}
+
+#sub get_instance {
+#    return
+#}
+
+1;
