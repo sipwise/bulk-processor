d01abbc99c44c3d0759301457c78eabd47e9c87b
Rene Krenn <rkrenn@sipwise.com>
2018-11-09 13:22:00 +0100

    TT#46754 ama "block" and "file" objects WIP
    Change-Id: Ie8d5730cc4eabbcc2c907ef49640414fdd4ede4e
    (cherry picked from commit 746742259aa2310edf5ba8b746a5a211684c0c84)

---
+++ lib/NGCP/BulkProcessor/Projects/Export/Ama/Format/Block.pm
@@ -0,0 +1,71 @@
+package NGCP::BulkProcessor::Projects::Export::Ama::Format::Block;
+use strict;
+
+## no critic
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(
+
+);
+
+my $block_descriptor_word_length = 8;
+my $max_block_length = 2048; #bytes
+
+sub new {
+
+    my $class = shift;
+    my $self = bless {}, $class;
+    $self->{records} = [];
+    $self->{padded} = 0;
+    $self->{length} = $block_descriptor_word_length;
+
+    return $self;
+
+}
+
+sub add_record {
+    my $self = shift;
+    my $record = shift;
+    my $length;
+    if (not $self->{padded} and ($length = $self->get_length() + $record->get_length()) <= 2 * $max_block_length) {
+        push(@{$self->{records}},$record);
+        $self->{length} = $length;
+        return 1;
+    }
+    return 0;
+
+}
+
+sub get_hex {
+
+    my $self = shift;
+    my $result = $self->_get_block_descriptor_word();
+    foreach my $record (@{$self->{records}}) {
+        $result .= $record->get_hex();
+    }
+    if ($self->{padded}) {
+        $result .= 'aa' x (2 * $max_block_length - length($result));
+    }
+    return $result;
+}
+
+
+sub get_length {
+    my $self = shift;
+    return $self->{length};
+}
+
+
+sub _get_block_descriptor_word {
+    my $self = shift;
+    return sprintf('%04x',$self->get_length() / 2) . '0000';
+    #return total length in bytes (up to 256*256 bytes)
+}
+
+sub set_padded {
+    my $self = shift;
+    $self->{padded} = shift;
+}
+
+1;
