fd31b6a991dcbd1657c67371f4b7a56ddc4a4056
Rene Krenn <rkrenn@sipwise.com>
2019-01-22 17:40:59 +0100

    TT#46753 ama: 611, 199, 104, 000 modules
    Change-Id: I3e1f201f4ce8c47b8836c75e5cd018e45b93756e

---
+++ lib/NGCP/BulkProcessor/Projects/Export/Ama/Format/Fields/ModuleCode.pm
@@ -0,0 +1,69 @@
+package NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::ModuleCode;
+use strict;
+
+## no critic
+
+use NGCP::BulkProcessor::Projects::Export::Ama::Format::Field qw($TERMINATOR);
+
+use NGCP::BulkProcessor::Array qw(contains);
+
+require Exporter;
+our @ISA = qw(Exporter NGCP::BulkProcessor::Projects::Export::Ama::Format::Field);
+our @EXPORT_OK = qw(
+    $MODULE_CODE_611
+    $MODULE_CODE_199
+    $MODULE_CODE_104
+    $MODULE_CODE_000
+);
+
+my $field_name = "module code";
+my $length = 4;
+my @param_names = qw/module_code/;
+
+my @module_codes = ();
+our $MODULE_CODE_611 = '611';
+push(@module_codes,$MODULE_CODE_611);
+our $MODULE_CODE_199 = '199';
+push(@module_codes,$MODULE_CODE_199);
+our $MODULE_CODE_104 = '104';
+push(@module_codes,$MODULE_CODE_104);
+our $MODULE_CODE_000 = '000';
+push(@module_codes,$MODULE_CODE_000);
+
+sub new {
+
+    my $class = shift;
+    my $self = NGCP::BulkProcessor::Projects::Export::Ama::Format::Field->new(
+        $class,
+        name => $field_name,
+        length => $length,
+        @_);
+
+    return $self;
+
+}
+
+sub _get_param_names {
+
+    my $self = shift;
+    return @param_names;
+
+}
+
+sub get_module_code {
+
+    my $self = shift;
+    return $self->{module_code};
+
+}
+
+sub get_hex {
+
+    my $self = shift;
+    my ($module_code) = $self->_get_params(@_);
+    die("invalid module code '$module_code'") unless contains($module_code,\@module_codes);
+    return $module_code . $TERMINATOR;
+
+}
+
+1;
