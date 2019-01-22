1b485505e725ce65de99508b188c8e56f6afaf1d
Rene Krenn <rkrenn@sipwise.com>
2019-01-22 17:40:59 +0100

    TT#46753 ama: 611, 199, 104 and 000 modules
    (cherry picked from commit 2638f6e091d1fc0e2960353be7691d26dde51d36)
    
    Change-Id: I35ebe03fec809cc97eeb5bc674cf82a8b9158e75

---
+++ lib/NGCP/BulkProcessor/Projects/Export/Ama/Format/Fields/DataDescriptor.pm
@@ -0,0 +1,50 @@
+package NGCP::BulkProcessor::Projects::Export::Ama::Format::Fields::DataDescriptor;
+use strict;
+
+## no critic
+
+use NGCP::BulkProcessor::Projects::Export::Ama::Format::Field qw($TERMINATOR);
+
+#use NGCP::BulkProcessor::Array qw(contains);
+
+require Exporter;
+our @ISA = qw(Exporter NGCP::BulkProcessor::Projects::Export::Ama::Format::Field);
+our @EXPORT_OK = qw(
+
+);
+
+my $field_name = "data descriptor";
+my $length = 4;
+my @param_names = qw/data_descriptor/;
+
+sub new {
+
+    my $class = shift;
+    my $self = NGCP::BulkProcessor::Projects::Export::Ama::Format::Field->new(
+        $class,
+        name => $field_name,
+        length => $length,
+        @_);
+    $self->{data_descriptor} //= 0;
+
+    return $self;
+
+}
+
+sub _get_param_names {
+
+    my $self = shift;
+    return @param_names;
+
+}
+
+sub get_hex {
+
+    my $self = shift;
+    my ($data_descriptor) = $self->_get_params(@_);
+    die("invalid data descriptor '$data_descriptor'") if (length($data_descriptor) < 1 or length($data_descriptor) > 3);
+    return sprintf('%03d',$data_descriptor) . '1'; # . $TERMINATOR;
+
+}
+
+1;
