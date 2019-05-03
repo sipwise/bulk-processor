ac6ab115f1b4966040cfdbb1a52b06f7b470a9fc
Rene Krenn <rkrenn@sipwise.com>
2019-05-03 12:18:51 +0200

    TT#58651 ama exporter: add cfu_no_ivr scenario WIP
    Change-Id: I58ed4c428632b4bac1ad3b701650a77cd29233e8

--- lib/NGCP/BulkProcessor/Dao/Trunk/accounting/cdr.pm
+++ lib/NGCP/BulkProcessor/Dao/Trunk/accounting/cdr.pm
@@ -47,6 +47,8 @@ our @EXPORT_OK = qw(
 
     $OK_CALL_STATUS
 
+    $CFU_CALL_TYPE
+
 );
 #process_records
 #delete_ids
@@ -150,6 +152,8 @@ my $XFERSUFFIX = '_xfer-1';
 push(@callid_suffixes,$XFERSUFFIX);
 
 our $OK_CALL_STATUS = 'ok';
+
+our $CFU_CALL_TYPE = 'cfu';
 
 my $indexes = {};
 
