7b66b31a4ad0e737eb71364d98facf7abed6c902
Rene Krenn <rkrenn@sipwise.com>
2019-07-29 13:37:07 +0200

    TT#64001 .ama: process User-To-User instead of $ivr_duration_limit
    Change-Id: Ib118e8f543cf67fabde75e12bdda71653df277f8

---
+++ lib/NGCP/BulkProcessor/Dao/Trunk/accounting/cdr_provider.pm
@@ -0,0 +1,117 @@
+package NGCP::BulkProcessor::Dao::Trunk::accounting::cdr_provider;
+use strict;
+
+## no critic
+
+use NGCP::BulkProcessor::Logging qw(
+    getlogger
+);
+
+use NGCP::BulkProcessor::ConnectorPool qw(
+    get_accounting_db
+    destroy_dbs
+);
+
+use NGCP::BulkProcessor::SqlProcessor qw(
+    checktableinfo
+    copy_row
+
+);
+use NGCP::BulkProcessor::SqlRecord qw();
+
+require Exporter;
+our @ISA = qw(Exporter NGCP::BulkProcessor::SqlRecord);
+our @EXPORT_OK = qw(
+    gettablename
+    check_table
+
+    findall
+
+    $CUSTOMER
+    $RESELLER
+    $CARRIER
+
+);
+
+
+my $tablename = 'cdr_provider';
+my $get_db = \&get_accounting_db;
+
+my $expected_fieldnames = [
+  "id",
+  "type",
+];
+
+our $CUSTOMER = 'customer';
+our $RESELLER = 'reseller';
+our $CARRIER = 'carrier';
+
+my $indexes = {};
+
+my $insert_unique_fields = [];
+
+sub new {
+
+    my $class = shift;
+    my $self = NGCP::BulkProcessor::SqlRecord->new($class,$get_db,
+                           $tablename,$expected_fieldnames,$indexes);
+
+    copy_row($self,shift,$expected_fieldnames);
+
+    return $self;
+
+}
+
+sub findall {
+
+    my ($load_recursive) = @_;
+
+    check_table();
+    my $db = &$get_db();
+    my $table = $db->tableidentifier($tablename);
+
+    my $stmt = 'SELECT * FROM ' . $table;
+    my @params = ();
+    my $rows = $xa_db->db_get_all_arrayref($stmt,@params);
+
+    return buildrecords_fromrows($rows,$load_recursive);
+
+}
+
+sub buildrecords_fromrows {
+
+    my ($rows,$load_recursive) = @_;
+
+    my @records = ();
+    my $record;
+
+    if (defined $rows and ref $rows eq 'ARRAY') {
+        foreach my $row (@$rows) {
+            $record = __PACKAGE__->new($row);
+
+            # transformations go here ...
+
+            push @records,$record;
+        }
+    }
+
+    return \@records;
+
+}
+
+sub gettablename {
+
+    return $tablename;
+
+}
+
+sub check_table {
+
+    return checktableinfo($get_db,
+                   __PACKAGE__,$tablename,
+                   $expected_fieldnames,
+                   $indexes);
+
+}
+
+1;
