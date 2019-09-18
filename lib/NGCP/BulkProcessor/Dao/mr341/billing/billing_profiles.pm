dfe8ee9aaed976b75fd45e05667e8fab11390956
Rene Krenn <rkrenn@sipwise.com>
2019-09-18 15:03:16 +0200

    TT#67109 commconcept mergemigration tool WIP
    Change-Id: I8db0588c43fd96cdf7f6d9901773b29e940cae16

---
+++ lib/NGCP/BulkProcessor/Dao/mr341/billing/billing_profiles.pm
@@ -0,0 +1,140 @@
+package NGCP::BulkProcessor::Dao::mr341::billing::billing_profiles;
+use strict;
+
+## no critic
+
+use NGCP::BulkProcessor::ConnectorPool qw(
+    get_billing_db
+
+);
+
+use NGCP::BulkProcessor::SqlProcessor qw(
+    checktableinfo
+    copy_row
+);
+use NGCP::BulkProcessor::SqlRecord qw();
+
+require Exporter;
+our @ISA = qw(Exporter NGCP::BulkProcessor::SqlRecord);
+our @EXPORT_OK = qw(
+    gettablename
+    check_table
+
+
+
+    source_findby_resellerid
+);
+
+my $tablename = 'billing_profiles';
+my $get_db = \&get_billing_db;
+
+my $expected_fieldnames = [
+    'id',
+    'reseller_id',
+    'handle',
+    'name',
+    'prepaid',
+    'interval_charge',
+    'interval_free_time',
+    'interval_free_cash',
+    'interval_unit',
+    'interval_count',
+    'fraud_interval_limit',
+    'fraud_interval_lock',
+    'fraud_interval_notify',
+    'fraud_daily_limit',
+    'fraud_daily_lock',
+    'fraud_daily_notify',
+    #'fraud_use_reseller_rates',
+    'currency',
+    #'status',
+    #'modify_timestamp',
+    #'create_timestamp',
+    #'terminate_timestamp',
+];
+
+my $indexes = {};
+
+sub new {
+
+    my $class = shift;
+    my $self = NGCP::BulkProcessor::SqlRecord->new($class,$get_db,
+                           $tablename,$expected_fieldnames,$indexes);
+
+    copy_row($self,shift,$expected_fieldnames);
+
+    return $self;
+
+}
+
+
+
+sub gettablename {
+
+    return $tablename;
+
+}
+
+sub check_table {
+
+    return checktableinfo(shift // $get_db,
+                   __PACKAGE__,$tablename,
+                   $expected_fieldnames,
+                   $indexes);
+
+}
+
+sub source_new {
+
+    my $class = shift;
+    my $self = NGCP::BulkProcessor::SqlRecord->new($class,shift,
+                           $tablename,$expected_fieldnames,$indexes);
+
+    copy_row($self,shift,$expected_fieldnames);
+
+    return $self;
+
+}
+
+sub source_findby_resellerid {
+
+    my ($source_dbs,$reseller_id) = @_;
+
+    my $source_db = $source_dbs->{billing_db};
+    check_table($source_db);
+    my $db = &$source_db();
+    my $table = $db->tableidentifier($tablename);
+
+    my $stmt = 'SELECT * FROM ' . $table . ' WHERE ' .
+            $db->columnidentifier('reseller_id') . ' = ?';
+    my @params = ($reseller_id);
+
+    my $rows = $db->db_get_all_arrayref($stmt,@params);
+
+    return source_buildrecords_fromrows($rows,$source_dbs);
+
+}
+
+sub source_buildrecords_fromrows {
+
+    my ($rows,$source_dbs) = @_;
+
+    my @records = (); # : shared = ();
+    my $record;
+
+    if (defined $rows and ref $rows eq 'ARRAY') {
+        foreach my $row (@$rows) {
+            $record = __PACKAGE__->source_new($source_dbs->{billing_db},$row);
+
+            # transformations go here ...
+
+
+            push @records,$record;
+        }
+    }
+
+    return \@records;
+
+}
+
+1;
