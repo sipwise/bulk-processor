59ba7b306a4f134529e5562c9981d67431c0eed9
Rene Krenn <rkrenn@sipwise.com>
2019-09-18 15:03:16 +0200

    TT#67109 commconcept mergemigration tool #1
    Change-Id: I8db0588c43fd96cdf7f6d9901773b29e940cae16

--- lib/NGCP/BulkProcessor/Dao/mr38/billing/contracts.pm
+++ lib/NGCP/BulkProcessor/Dao/mr38/billing/contracts.pm
@@ -1,6 +1,8 @@
 package NGCP::BulkProcessor::Dao::mr38::billing::contracts;
 use strict;
 
+use threads::shared;
+
 ## no critic
 
 use NGCP::BulkProcessor::Logging qw(
@@ -22,6 +24,11 @@ use NGCP::BulkProcessor::SqlProcessor qw(
 );
 use NGCP::BulkProcessor::SqlRecord qw();
 
+use NGCP::BulkProcessor::Dao::mr38::billing::billing_mappings qw();
+use NGCP::BulkProcessor::Dao::mr38::billing::contacts qw();
+use NGCP::BulkProcessor::Dao::mr38::billing::contract_balances qw();
+use NGCP::BulkProcessor::Dao::mr38::billing::voip_subscribers qw();
+
 require Exporter;
 our @ISA = qw(Exporter NGCP::BulkProcessor::SqlRecord);
 our @EXPORT_OK = qw(
@@ -32,6 +39,7 @@ our @EXPORT_OK = qw(
     countby_status_resellerid
 
     process_records
+    source_process_records
 
     $ACTIVE_STATE
     $TERMINATED_STATE
@@ -181,7 +189,6 @@ sub process_records {
     );
 }
 
-
 sub buildrecords_fromrows {
 
     my ($rows,$load_recursive) = @_;
@@ -211,10 +218,96 @@ sub gettablename {
 
 sub check_table {
 
-    return checktableinfo($get_db,
+    return checktableinfo(shift // $get_db,
                    __PACKAGE__,$tablename,
                    $expected_fieldnames,
                    $indexes);
+
+}
+
+sub source_new {
+
+    my $class = shift;
+    my $self = NGCP::BulkProcessor::SqlRecord->new_shared($class,shift,
+                           $tablename,$expected_fieldnames,$indexes);
+
+    copy_row($self,shift,$expected_fieldnames);
+
+    return $self;
+
+}
+
+sub source_process_records {
+
+    my %params = @_;
+    my ($source_dbs,
+        $process_code,
+        $read_code,
+        $static_context,
+        $init_process_context_code,
+        $uninit_process_context_code,
+        $multithreading,
+        $numofthreads) = @params{qw/
+            source_dbs
+            process_code
+            read_code
+            static_context
+            init_process_context_code
+            uninit_process_context_code
+            multithreading
+            numofthreads
+        /};
+
+    my $source_db = $source_dbs->{billing_db};
+    check_table($source_db);
+    my $db = &$source_db();
+    my $table = $db->tableidentifier($tablename);
+
+    return process_table(
+        get_db                      => $source_db,
+        class                       => __PACKAGE__,
+        process_code                => sub {
+                my ($context,$rowblock,$row_offset) = @_;
+                #return &$process_code($context,buildrecords_fromrows_source($rowblock,$source_db,$load_recursive),$row_offset);
+                return &$process_code($context,$rowblock,$row_offset);
+            },
+        read_code                => sub {
+                my ($rowblock) = @_;
+                return source_buildrecords_fromrows($rowblock,$source_dbs);
+            },
+        static_context              => $static_context,
+        init_process_context_code   => $init_process_context_code,
+        uninit_process_context_code => $uninit_process_context_code,
+        destroy_reader_dbs_code     => \&destroy_dbs,
+        multithreading              => $multithreading,
+        tableprocessing_threads     => $numofthreads,
+        'select'                    => 'SELECT * FROM ' . $table . ' WHERE ' . $db->columnidentifier('status') . ' != "' . $TERMINATED_STATE . '"',
+        'selectcount'               => 'SELECT COUNT(*) FROM ' . $table . ' WHERE ' . $db->columnidentifier('status') . ' != "' . $TERMINATED_STATE . '"',
+    );
+}
+
+sub source_buildrecords_fromrows {
+
+    my ($rows,$source_dbs) = @_;
+
+    my @records : shared = ();
+    my $record;
+
+    if (defined $rows and ref $rows eq 'ARRAY') {
+        foreach my $row (@$rows) {
+            $record = __PACKAGE__->source_new($source_dbs->{billing_db},$row);
+
+            $record->{billing_mappings} = NGCP::BulkProcessor::Dao::mr38::billing::billing_mappings::source_findby_contractid($source_dbs,$record->{id});
+            $record->{contact} = NGCP::BulkProcessor::Dao::mr38::billing::contacts::source_findby_id($source_dbs,$record->{id});
+            $record->{contract_balances} = NGCP::BulkProcessor::Dao::mr38::billing::contract_balances::source_findby_contractid($source_dbs,$record->{id});
+            #contract_fraud_preferences
+            $record->{voip_subscribers} = NGCP::BulkProcessor::Dao::mr38::billing::voip_subscribers::source_findby_contractid($source_dbs,$record->{id});
+
+            push @records,$record;
+        }
+    }
+
+    return \@records;
 
 }
 
