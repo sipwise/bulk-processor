63b4db29059ec673037bd6a6d2b7b4fe0d0e3990
Rene Krenn <rkrenn@sipwise.com>
2019-09-18 15:03:16 +0200

    TT#67109 commconcept mergemigration tool WIP
    Change-Id: I8db0588c43fd96cdf7f6d9901773b29e940cae16

---
+++ lib/NGCP/BulkProcessor/Dao/mr341/provisioning/voip_usr_preferences.pm
@@ -0,0 +1,134 @@
+package NGCP::BulkProcessor::Dao::mr341::provisioning::voip_usr_preferences;
+use strict;
+
+## no critic
+
+use threads::shared;
+
+use NGCP::BulkProcessor::Logging qw(
+    getlogger
+);
+
+use NGCP::BulkProcessor::ConnectorPool qw(
+    get_provisioning_db
+);
+
+use NGCP::BulkProcessor::SqlProcessor qw(
+    checktableinfo
+    copy_row
+);
+use NGCP::BulkProcessor::SqlRecord qw();
+
+require Exporter;
+our @ISA = qw(Exporter NGCP::BulkProcessor::SqlRecord);
+our @EXPORT_OK = qw(
+    gettablename
+    check_table
+
+    findby_subscriberid_attributeid
+    countby_subscriberid_attributeid_value
+
+    $TRUE
+    $FALSE
+);
+
+my $tablename = 'voip_usr_preferences';
+my $get_db = \&get_provisioning_db;
+
+my $expected_fieldnames = [
+    'id',
+    'subscriber_id',
+    'attribute_id',
+    'value',
+    'modify_timestamp',
+];
+
+my $indexes = {};
+
+my $insert_unique_fields = [];
+
+our $TRUE = 1;
+our $FALSE = undef;
+
+sub new {
+
+    my $class = shift;
+    my $self = NGCP::BulkProcessor::SqlRecord->new($class,$get_db,
+                           $tablename,$expected_fieldnames,$indexes);
+
+    copy_row($self,shift,$expected_fieldnames);
+
+    return $self;
+
+}
+
+sub gettablename {
+
+    return $tablename;
+
+}
+
+sub check_table {
+
+    return checktableinfo(shift // $get_db,
+                   __PACKAGE__,$tablename,
+                   $expected_fieldnames,
+                   $indexes);
+
+}
+
+sub source_new {
+
+    my $class = shift;
+    my $self = NGCP::BulkProcessor::SqlRecord->new_shared($class,shift,
+                           $tablename,$expected_fieldnames,$indexes);
+
+    copy_row($self,shift,$expected_fieldnames);
+
+    return $self;
+
+}
+
+sub source_findby_subscriberid {
+
+    my ($source_dbs,$subscriber_id) = @_;
+
+    my $source_db = $source_dbs->{provisioning_db};
+    check_table($source_db);
+    my $db = &$source_db();
+    my $table = $db->tableidentifier($tablename);
+
+    my $stmt = 'SELECT v.*,a.attribute FROM ' . $table . ' v JOIN ' .
+            $db->tableidentifier('voip_preferences') . ' a ON v.attribute_id = a.id WHERE ' .
+            'v.subscriber_id = ?';
+    my @params = ($subscriber_id);
+
+    my $rows = $db->db_get_all_arrayref($stmt,@params);
+
+    return source_buildrecords_fromrows($rows,$source_dbs);
+
+}
+
+sub source_buildrecords_fromrows {
+
+    my ($rows,$source_dbs) = @_;
+
+    my @records : shared = ();
+    my $record;
+
+    if (defined $rows and ref $rows eq 'ARRAY') {
+        foreach my $row (@$rows) {
+            $record = __PACKAGE__->source_new($source_dbs->{provisioning_db},$row);
+
+            # transformations go here ...
+            $record->{attribute} = $row->{attribute};
+
+            push @records,$record;
+        }
+    }
+
+    return \@records;
+
+}
+
+1;
