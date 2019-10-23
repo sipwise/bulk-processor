af042ba7cabf8f5901bbe25b8ba78ce76a456d8a
Rene Krenn <rkrenn@sipwise.com>
2019-10-23 12:29:32 +0200

    TT#69125 mr1.0.3 source db schema
    Change-Id: I35b67c66236696af6c041e34dc4a732fdca1069d
    (cherry picked from commit f763ad109c952cd1550cd64b7786263de79f9870)

--- lib/NGCP/BulkProcessor/Dao/mr103/billing/domain_resellers.pm
+++ lib/NGCP/BulkProcessor/Dao/mr103/billing/domain_resellers.pm
@@ -1,16 +1,11 @@
-package NGCP::BulkProcessor::Dao::mr553::billing::voip_numbers;
+package NGCP::BulkProcessor::Dao::mr103::billing::domain_resellers;
 use strict;
 
 ## no critic
 
-use threads::shared;
-
-use NGCP::BulkProcessor::Logging qw(
-    getlogger
-);
-
 use NGCP::BulkProcessor::ConnectorPool qw(
     get_billing_db
+
 );
 
 use NGCP::BulkProcessor::SqlProcessor qw(
@@ -19,35 +14,27 @@ use NGCP::BulkProcessor::SqlProcessor qw(
 );
 use NGCP::BulkProcessor::SqlRecord qw();
 
+use NGCP::BulkProcessor::Dao::mr103::billing::domains qw();
+
 require Exporter;
 our @ISA = qw(Exporter NGCP::BulkProcessor::SqlRecord);
 our @EXPORT_OK = qw(
     gettablename
     check_table
 
-    source_findby_subscriberid
-    source_findby_id
-
+    source_findby_resellerid
 );
 
-my $tablename = 'voip_numbers';
+my $tablename = 'domain_resellers';
 my $get_db = \&get_billing_db;
 
 my $expected_fieldnames = [
     'id',
-    'cc',
-    'ac',
-    'sn',
+    'domain_id',
     'reseller_id',
-    'subscriber_id',
-    'status',
-    'ported',
-    'list_timestamp',
 ];
 
 my $indexes = {};
-
-our $ACTIVE_STATE = 'active';
 
 sub new {
 
@@ -61,27 +48,6 @@ sub new {
 
 }
 
-
-sub buildrecords_fromrows {
-
-    my ($rows,$load_recursive) = @_;
-
-    my @records = ();
-    my $record;
-
-    if (defined $rows and ref $rows eq 'ARRAY') {
-        foreach my $row (@$rows) {
-            $record = __PACKAGE__->new($row);
-
-            # transformations go here ...
-
-            push @records,$record;
-        }
-    }
-
-    return \@records;
-
-}
 
 sub gettablename {
 
@@ -101,7 +67,7 @@ sub check_table {
 sub source_new {
 
     my $class = shift;
-    my $self = NGCP::BulkProcessor::SqlRecord->new_shared($class,shift,
+    my $self = NGCP::BulkProcessor::SqlRecord->new($class,shift,
                            $tablename,$expected_fieldnames,$indexes);
 
     copy_row($self,shift,$expected_fieldnames);
@@ -110,26 +76,7 @@ sub source_new {
 
 }
 
-sub source_findby_subscriberid {
-
-    my ($source_dbs,$subscriber_id) = @_;
-
-    my $source_db = $source_dbs->{billing_db};
-    check_table($source_db);
-    my $db = &$source_db();
-    my $table = $db->tableidentifier($tablename);
-
-    my $stmt = 'SELECT * FROM ' . $table . ' WHERE ' .
-            $db->columnidentifier('subscriber_id') . ' = ?';
-    my @params = ($subscriber_id);
-
-    my $rows = $db->db_get_all_arrayref($stmt,@params);
-
-    return source_buildrecords_fromrows($rows,$source_dbs);
-
-}
-
-sub source_findby_id {
+sub source_findby_resellerid {
 
     my ($source_dbs,$id) = @_;
 
@@ -139,12 +86,12 @@ sub source_findby_id {
     my $table = $db->tableidentifier($tablename);
 
     my $stmt = 'SELECT * FROM ' . $table . ' WHERE ' .
-            $db->columnidentifier('id') . ' = ?';
-    my @params = ($id);
+            $db->columnidentifier('reseller_id') . ' = ?';
 
+    my @params = ($id);
     my $rows = $db->db_get_all_arrayref($stmt,@params);
 
-    return source_buildrecords_fromrows($rows,$source_dbs)->[0];
+    return source_buildrecords_fromrows($rows,$source_dbs);
 
 }
 
@@ -152,16 +99,14 @@ sub source_buildrecords_fromrows {
 
     my ($rows,$source_dbs) = @_;
 
-    my @records : shared = ();
+    my @records = (); # : shared = ();
     my $record;
 
     if (defined $rows and ref $rows eq 'ARRAY') {
         foreach my $row (@$rows) {
             $record = __PACKAGE__->source_new($source_dbs->{billing_db},$row);
 
-            # transformations go here ...
-
-            #$record->{provisioning_voip_subscriber} = NGCP::BulkProcessor::Dao::mr341::provisioning::voip_subscribers::source_findby_uuid($source_dbs,$record->{uuid});
+            $record->{domain} = NGCP::BulkProcessor::Dao::mr103::billing::domains::source_findby_id($source_dbs,$record->{domain_id});
 
             push @records,$record;
         }
