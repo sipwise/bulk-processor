e49ef78b0532a67e3fbafbe3b050e44535bfe245
Rene Krenn <rkrenn@sipwise.com>
2020-07-29 22:27:09 +0200

    TT#87561 openser/ngcp mr1.0.2 dao db schema
    Change-Id: I37a174b71c80bc24d9246eafc20a12e18256d736

---
+++ lib/NGCP/BulkProcessor/Dao/mr102/billing/ncos_pattern_list.pm
@@ -0,0 +1,142 @@
+package NGCP::BulkProcessor::Dao::mr102::billing::ncos_pattern_list;
+use strict;
+
+## no critic
+
+#use threads::shared;
+
+use NGCP::BulkProcessor::ConnectorPool qw(
+    get_billing_db
+);
+
+use NGCP::BulkProcessor::SqlProcessor qw(
+    checktableinfo
+    copy_row
+);
+use NGCP::BulkProcessor::SqlRecord qw();
+
+require Exporter;
+our @ISA = qw(Exporter NGCP::BulkProcessor::SqlRecord);
+our @EXPORT_OK = qw(
+    gettablename
+    check_table
+
+    source_findby_ncoslevelid
+);
+
+my $tablename = 'ncos_pattern_list';
+my $get_db = \&get_billing_db;
+
+my $expected_fieldnames = [
+  'id',
+  'ncos_level_id',
+  'pattern',
+  'description',
+];
+
+my $indexes = {};
+
+my $insert_unique_fields = [];
+
+sub new {
+
+    my $class = shift;
+    my $self = NGCP::BulkProcessor::SqlRecord->new($class,$get_db,
+                           $tablename,$expected_fieldnames,$indexes);
+
+    copy_row($self,shift,$expected_fieldnames);
+
+    return $self;
+
+}
+
+sub buildrecords_fromrows {
+
+    my ($rows,$load_recursive) = @_;
+
+    my @records = ();
+    my $record;
+
+    if (defined $rows and ref $rows eq 'ARRAY') {
+        foreach my $row (@$rows) {
+            $record = __PACKAGE__->new($row);
+
+            # transformations go here ...
+
+            push @records,$record;
+        }
+    }
+
+    return \@records;
+
+}
+
+sub gettablename {
+
+    return $tablename;
+
+}
+
+sub check_table {
+
+    return checktableinfo($get_db,
+                   __PACKAGE__,$tablename,
+                   $expected_fieldnames,
+                   $indexes);
+
+}
+
+sub source_new {
+
+    my $class = shift;
+    my $self = NGCP::BulkProcessor::SqlRecord->new($class,shift,
+                           $tablename,$expected_fieldnames,$indexes);
+
+    copy_row($self,shift,$expected_fieldnames);
+
+    return $self;
+
+}
+
+sub source_findby_ncoslevelid {
+
+    my ($source_dbs,$ncos_level_id) = @_;
+
+    my $source_db = $source_dbs->{billing_db};
+    check_table($source_db);
+    my $db = &$source_db();
+    my $table = $db->tableidentifier($tablename);
+
+    my $stmt = 'SELECT * FROM ' . $table . ' WHERE ' .
+            $db->columnidentifier('ncos_level_id') . ' = ?';
+    my @params = ($ncos_level_id);
+
+    my $rows = $db->db_get_all_arrayref($stmt,@params);
+
+    return source_buildrecords_fromrows($rows,$source_dbs);
+
+}
+
+sub source_buildrecords_fromrows {
+
+    my ($rows,$source_dbs) = @_;
+
+    my @records = (); # : shared = ();
+    my $record;
+
+    if (defined $rows and ref $rows eq 'ARRAY') {
+        foreach my $row (@$rows) {
+            $record = __PACKAGE__->source_new($source_dbs->{billing_db},$row);
+
+            # transformations go here ...
+
+
+            push @records,$record;
+        }
+    }
+
+    return \@records;
+
+}
+
+1;
